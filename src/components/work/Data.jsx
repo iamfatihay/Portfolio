73f1c89844fa1d4d53059842a37c8921467008a2
Fatih <16fay61@gmail.com>
2023-08-09 16:48:02 +0200

    aa

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,5 +1,5 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work1 from "../../assets/blog-app.gif";
+import Work2 from "../../assets/stockApp.gif";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
@@ -9,31 +9,37 @@ export const projectsData = [
     {
       id: 1,
       image: Work1,
-      title: "Web design",
-      category: "web",
+      title: "Blog App",
+      link: "https://fatihay-blog-app.vercel.app/",
+      link2:"https://github.com/iamfatihay/Blog-App",
+      category: "Advanced",
     },
     {
       id: 2,
       image: Work2,
-      title: "App movil",
+      title: "Stock App",
+      link: "",
       category: "app",
     },
     {
       id: 3,
       image: Work3,
       title: "Brand design",
+      link: "",
       category: "design",
     },
     {
       id: 4,
       image: Work4,
       title: "App movil",
+      link: "",
       category: "app",
     },
     {
       id: 5,
       image: Work5,
       title: "Web design",
+      link: "",
       category: "Web",
     },
   ];
@@ -43,7 +49,7 @@ export const projectsNav = [
         name:"all",
     },
     {
-        name:"Web",
+        name:"React / Redux",
     },
     {
         name:"app",
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
 const WorksItems = ({ item }) => {
+    // const handleClick = () => {
+    //     navigate(item.link)
+    // }
+
     return (
         <div className="work__card" key={item.id} >
-            <img src={item.image} alt="" className='work__img' />
-            <h3 className="work__title">{item.title}</h3>
-            <a href="#home" className="work__button">
-                Demo 
-                 <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            <a href={item.link}>
+                <img src={item.image} alt="" className='work__img'/>
             </a>
+
+            <h3 className="work__title">{item.title}</h3>
+            <div className='links' >
+                <a href={item.link} className="work__button" target='_blank' rel="noreferrer">
+                    Live link
+                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                </a>
+                <a href={item.link2} className="work__button" target='_blank' rel="noreferrer">
+                    GitHub link
+                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                </a>
+            </div>
         </div>
     )
 }
