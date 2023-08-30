98f12bce799663dd9c8e0298e183e4913f540626
Fatih <16fay61@gmail.com>
2023-08-30 17:21:23 +0200

    certificates added

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -18,15 +18,21 @@ const Qualification = () => {
                 <div className="qualification__tabs">
                     <div className={toggleState === 1 ? "qualification__button qualification__active button--flex"
                         : "qualification__button button--flex"}
-                        onClick={()=>toggleTab(1)}
-                         >
+                        onClick={() => toggleTab(1)}
+                    >
                         <i className="uil uil-graduation-cap qualification__icon"></i>Education
                     </div>
                     <div className={toggleState === 2 ? "qualification__button qualification__active button--flex"
                         : "qualification__button button--flex"}
-                        onClick={()=>toggleTab(2)}
-                        >
+                        onClick={() => toggleTab(2)}
+                    >
                         <i className="uil uil-briefcase-alt qualification__icon"></i>Experience
+                    </div>
+                    <div className={toggleState === 3 ? "qualification__button qualification__active button--flex"
+                        : "qualification__button button--flex"}
+                        onClick={() => toggleTab(3)}
+                    >
+                        <i className="uil uil-briefcase-alt qualification__icon"></i>Certificates
                     </div>
                 </div>
 
@@ -38,7 +44,7 @@ const Qualification = () => {
                                 <h3 className="qualification__title">Full Stack Developer</h3>
                                 <span className="qualification__subtitle">Stuttgart - Clarusway </span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2022 - present
+                                    <i className="uil uil-calendar-alt"></i> 2022 - present
                                 </div>
                             </div>
                             <div>
@@ -58,7 +64,7 @@ const Qualification = () => {
                                 <h3 className="qualification__title">International Economic Policy</h3>
                                 <span className="qualification__subtitle">Bursa - Bursa Technical University,  Master's degree</span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2018 - paused
+                                    <i className="uil uil-calendar-alt"></i> 2018 - paused
                                 </div>
                             </div>
 
@@ -69,7 +75,7 @@ const Qualification = () => {
                                 <h3 className="qualification__title">Public Administration</h3>
                                 <span className="qualification__subtitle">Kütahya - Dumlupinar University,  Bachelor's degree</span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2012 - 2016
+                                    <i className="uil uil-calendar-alt"></i> 2012 - 2016
                                 </div>
                             </div>
                             <div>
@@ -103,7 +109,7 @@ const Qualification = () => {
                                 <h3 className="qualification__title">Full Stack Developer</h3>
                                 <span className="qualification__subtitle">Stuttgart - Clarusway, Trainee </span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2022 - present
+                                    <i className="uil uil-calendar-alt"></i> 2022 - present
                                 </div>
                             </div>
                             <div>
@@ -123,7 +129,7 @@ const Qualification = () => {
                                 <h3 className="qualification__title">Shipping and Invoice Manager</h3>
                                 <span className="qualification__subtitle">Ankara - Misbaşak </span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2018 - 2019
+                                    <i className="uil uil-calendar-alt"></i> 2018 - 2019
                                 </div>
                             </div>
 
@@ -134,7 +140,110 @@ const Qualification = () => {
                                 <h3 className="qualification__title">Manager</h3>
                                 <span className="qualification__subtitle">Bursa - htc home collection </span>
                                 <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>2016 - 2017
+                                    <i className="uil uil-calendar-alt"></i> 2016 - 2017
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className={toggleState === 3 ? "qualification__content qualification__content-active"
+                        : "qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Frontend Developer</h3>
+                                {/* <span className="qualification__subtitle">Clarusway</span> */}
+                                <a className='qualification__link' href="https://c11n.clarusway.com/en/verify/12825043385694?ref=email" target='_blank' rel="noreferrer">
+                                    Credential
+                                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                                </a>
+
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2023
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">JavaScript</h3>
+                                {/* <span className="qualification__subtitle">Clarusway</span> */}
+                                <a className='qualification__link' href="https://c11n.clarusway.com/en/verify/46897143464598?ref=email" target='_blank' rel="noreferrer">
+                                    Credential
+                                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                                </a>
+
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2023
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">React</h3>
+                                {/* <span className="qualification__subtitle">Clarusway</span> */}
+                                <a className='qualification__link' href="https://c11n.clarusway.com/en/verify/33670067149733?ref=email" target='_blank' rel="noreferrer">
+                                    Credential
+                                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                                </a>
+
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2023
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+                        <div className="qualification__data">
+                            <div></div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">HTML & CSS</h3>
+                                {/* <span className="qualification__subtitle">Clarusway</span> */}
+                                <a className='qualification__link' href="https://c11n.clarusway.com/en/verify/30228568874076?ref=email" target='_blank' rel="noreferrer">
+                                    Credential
+                                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                                </a>
+
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2023
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Python Data Structures</h3>
+                                {/* <span className="qualification__subtitle">Clarusway</span> */}
+                                <a className='qualification__link' href="https://www.sololearn.com/certificates/CT-9PVLYMVL" target='_blank' rel="noreferrer">
+                                    Credential
+                                    <i className="bx bx-right-arrow-alt work__button-icon"></i>
+                                </a>
+
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2023
                                 </div>
                             </div>
                             <div>
