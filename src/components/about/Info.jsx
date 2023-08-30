9ffc41bbe52db16c4b2918c371c8db741b59c9a1
Fatih <16fay61@gmail.com>
2023-08-30 18:10:57 +0200

    about updated

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -5,19 +5,18 @@ const Info = () => {
     <div className="about__info grid">
       <div className="about__box">
         <i class='bx bx-award about__icon'></i>
-
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">3+ Years</span>
       </div>
+
       <div className="about__box">
         <i class='bx bx-briefcase-alt about__icon' ></i>
-
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">15+ Projects</span>
       </div>
+      
       <div className="about__box">
         <i class='bx bx-code-alt about__icon'></i>
-
         <h3 className="about__title">Expertise</h3>
         <span className="about__subtitle">Full Stack</span>
       </div>
