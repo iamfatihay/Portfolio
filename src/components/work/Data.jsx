55e0aeb322cfc5606972c8feb991161bf61cefdd
Fatih <16fay61@gmail.com>
2025-10-03 15:33:41 +0200

    feat: Add Guess My Number game to portfolio
    - Add retro-styled number guessing game
    - Include game GIF preview
    - Add demo link (https://guess-number-xzmp.vercel.app/)
    - Add GitHub repository link
    - Category: Elementary (Vanilla JS game logic)

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -6,87 +6,94 @@ import Work5 from "../../assets/recipe.gif";
 import Work6 from "../../assets/countries.gif";
 import Work7 from "../../assets/nba.gif";
 import Work8 from "../../assets/anazon-store.gif";
-
-
+import Work9 from "../../assets/guess-number.gif";
 
 export const projectsData = [
-  {
-    id: 1,
-    image: Work1,
-    title: "Blog App",
-    link: "https://fatihay-blog-app.vercel.app/",
-    link2: "https://github.com/iamfatihay/Blog-App",
-    category: "Upper Intermediate",
-  },
-  {
-    id: 2,
-    image: Work2,
-    title: "Stock App",
-    link: "https://stock-app-fay.vercel.app/",
-    link2: "https://github.com/iamfatihay/Stock-app",
-    category: "Upper Intermediate",
-  },
-  {
-    id: 3,
-    image: Work3,
-    title: "Movie App",
-    link: "https://movie-app-react-fay.netlify.app/",
-    link2: "https://github.com/iamfatihay/Movie-app",
-    category: "Upper Intermediate",
-  },
-  {
-    id: 4,
-    image: Work4,
-    title: "Youtube-clone App",
-    link: "https://youtube-clone-app-fay.vercel.app/",
-    link2: "https://github.com/iamfatihay/youtube-clone-app",
-    category: "Intermediate",
-  },
-  {
-    id: 5,
-    image: Work5,
-    title: "Recipe App",
-    link: "https://recipe-app-fay.vercel.app/",
-    link2: "https://github.com/iamfatihay/Recipe-App",
-    category: "Intermediate",
-  },
-  {
-    id: 6,
-    image: Work6,
-    title: "Countries App",
-    link: "https://countries-react-app-fay.netlify.app/",
-    link2: "https://github.com/iamfatihay/Countries-App",
-    category: "Intermediate",
-  },
-  {
-    id: 7,
-    image: Work7,
-    title: "NBA-Legends App",
-    link: "https://nba-legends-app-fay.netlify.app/",
-    link2: "https://github.com/iamfatihay/NBA-legends-app",
-    category: "Elementary",
-  },
-  {
-    id: 8,
-    image: Work8,
-    title: "Anazon Store App",
-    link: "https://anazon-store-app.vercel.app",
-    link2: "https://github.com/iamfatihay/Anazon-storeApp",
-    category: "Elementary",
-  },
+    {
+        id: 1,
+        image: Work1,
+        title: "Blog App",
+        link: "https://fatihay-blog-app.vercel.app/",
+        link2: "https://github.com/iamfatihay/Blog-App",
+        category: "Upper Intermediate",
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Stock App",
+        link: "https://stock-app-fay.vercel.app/",
+        link2: "https://github.com/iamfatihay/Stock-app",
+        category: "Upper Intermediate",
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Movie App",
+        link: "https://movie-app-react-fay.netlify.app/",
+        link2: "https://github.com/iamfatihay/Movie-app",
+        category: "Upper Intermediate",
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Youtube-clone App",
+        link: "https://youtube-clone-app-fay.vercel.app/",
+        link2: "https://github.com/iamfatihay/youtube-clone-app",
+        category: "Intermediate",
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Recipe App",
+        link: "https://recipe-app-fay.vercel.app/",
+        link2: "https://github.com/iamfatihay/Recipe-App",
+        category: "Intermediate",
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "Countries App",
+        link: "https://countries-react-app-fay.netlify.app/",
+        link2: "https://github.com/iamfatihay/Countries-App",
+        category: "Intermediate",
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: "NBA-Legends App",
+        link: "https://nba-legends-app-fay.netlify.app/",
+        link2: "https://github.com/iamfatihay/NBA-legends-app",
+        category: "Elementary",
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: "Anazon Store App",
+        link: "https://anazon-store-app.vercel.app",
+        link2: "https://github.com/iamfatihay/Anazon-storeApp",
+        category: "Elementary",
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: "Guess My Number",
+        link: "https://guess-number-xzmp.vercel.app/",
+        link2: "https://github.com/iamfatihay/guess-number",
+        category: "Elementary",
+    },
 ];
 
 export const projectsNav = [
-  {
-    name: "all",
-  },
-  {
-    name: "Upper Intermediate",
-  },
-  {
-    name: "Intermediate",
-  },
-  {
-    name: "Elementary",
-  },
+    {
+        name: "all",
+    },
+    {
+        name: "Upper Intermediate",
+    },
+    {
+        name: "Intermediate",
+    },
+    {
+        name: "Elementary",
+    },
 ];
