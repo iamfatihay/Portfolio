abfcfa87f6c9071090f89906008493cd187b03be
Fatih <16fay61@gmail.com>
2025-10-03 15:04:20 +0200

    feat: Add current position at Hyfindr GmbH to achievements
    - Add "Currently Building Innovation At" section
    - Feature Lead Web Developer role at Hyfindr.com
    - Add gradient border and hover effects
    - Add clickable company link with icon animation
    - Position between Hackathon carousel and achievement details
    - Remove redundant info from About section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
-  return (
-    <div className="about__info grid">
-      <div className="about__box">
-        <i className='bx bx-award about__icon'></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">3+ Years</span>
-      </div>
+    return (
+        <div className="about__info grid">
+            <div className="about__box">
+                <i className="bx bx-award about__icon"></i>
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3+ Years</span>
+            </div>
 
-      <div className="about__box">
-        <i className='bx bx-briefcase-alt about__icon' ></i>
-        <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">15+ Projects</span>
-      </div>
-      
-      <div className="about__box">
-        <i className='bx bx-code-alt about__icon'></i>
-        <h3 className="about__title">Expertise</h3>
-        <span className="about__subtitle">Full Stack</span>
-      </div>
-    </div>
-  )
-}
+            <div className="about__box">
+                <i className="bx bx-briefcase-alt about__icon"></i>
+                <h3 className="about__title">Completed</h3>
+                <span className="about__subtitle">15+ Projects</span>
+            </div>
 
-export default Info
+            <div className="about__box">
+                <i className="bx bx-code-alt about__icon"></i>
+                <h3 className="about__title">Expertise</h3>
+                <span className="about__subtitle">Full Stack</span>
+            </div>
+        </div>
+    );
+};
+export default Info;
